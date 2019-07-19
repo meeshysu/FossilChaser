2114376dfba466cf0b5e7a51c03ba71687097ef5
Michelle Beshears <meeshysu@gmail.com>
2019-07-18 20:05:31 -0500

    user controller and repo created

---
+++ FossilChaser/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FossilChaser.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FossilChaser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [ApiController]
+        public class PaymentInformationController : ControllerBase
+        {
+            readonly UserRepository _repository;
+            readonly UserValidator _validator;
+
+            public PaymentInformationController(UserRepository repository)
+            {
+                _repository = repository;
+                _validator = new UserValidator();
+            }
+
+            //creating a new user
+            [HttpPost]
+            public ActionResult AddProduct(CreateUserRequest createRequest)
+            {
+                if (_validator.Validate(createRequest))
+                {
+                    return BadRequest("All product information must be filled out.");
+                }
+
+                var newProduct = _repository.AddProduct();
+
+                return Created($"api/user/{newUser.Id}", newUser);
+            }
+
+            //get all users
+            [HttpGet("getAllUsers")]
+            public ActionResult GetAllUsers()
+            {
+                var users = _repository.GetAll();
+                return Ok(users);
+            }
+
+            //get single user 
+            [HttpGet("getSingleUser/{id}")]
+            public ActionResult GetSingleProduct(int id)
+            {
+                var singleUser = _repository.GetSingleUser(id);
+                return Ok(singleUser);
+            }
+
+            //update user
+            [HttpPut("{id}")]
+            public ActionResult UpdateSingleProduct(User user)
+            {
+                var updateUser = _repository.UpdateUser(user);
+                return Ok(updateUser);
+            }
+
+            //delete user for admin purposes mostly
+            [HttpDelete("{id}")]
+            public ActionResult DeleteSingleUser(int id)
+            {
+                var deletedUser = _repository.DeleteSingleUser(id);
+                return Ok(deletedUser);
+            }
+        }
+
+        public class CreateProductRequestValidator
+        {
+            public bool Validate(CreateUserRequest requestToValidate)
+            {
+                return string.IsNullOrEmpty(requestToValidate.Title)
+                    || string.IsNullOrEmpty(requestToValidate.Description);
+            }
+        }
+    }
+}
---
+++ FossilChaser/Data/UserRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using FossilChaser.Models;
+
+namespace FossilChaser.Data
+{
+    public class UserRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=FossilChaser;Trusted_Connection=True;";
+
+        public User AddUser(int id, string userName, string password, string favorite)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newUser = db.QueryFirstOrDefault<User>(
+                                                                @"insert into User (id, userName, password, favorite)
+                                                                Output inserted.*
+                                                                values (@id, @userName, @password, @favorite)
+                                                                select * from User",
+                                                                new { id, userName, password, favorite });
+
+                if (User != null)
+                {
+                    return User;
+                }
+            }
+            throw new System.Exception("No new user found.");
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var users = db.Query<User>("select * from User").ToList();
+
+                return users;
+            }
+        }
+
+        public User GetSingleUsert(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var singleUser = db.QueryFirstOrDefault<User>(@"select *
+                                                                    from User
+                                                                    where id = @id",
+                                                                    new { id });
+                return singleUser;
+            }
+        }
+
+
+        public User UpdateSingleProduct(User singleUser)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var updatedUser = db.QueryFirstOrDefault<User>(@"update User
+                                        set userName = @userName,
+                                            password = @password,
+                                            favorite = @favorite,
+                                            output inserted.*
+                                            where id = @id",
+                                                            new
+                                                            {
+                                                                id = singleUser.Id,
+                                                                userName = singleUser.Username,
+                                                                password = singleUser.Password,
+                                                                favorite = singleUser.Favorite,
+                                                            });
+                return updatedUser;
+            }
+            throw new System.Exception("Could not update user.");
+        }
+
+        public User DeleteUser(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var deletedUser = db.QueryFirstOrDefault<User>(@"delete
+                                                                       from User
+                                                                       where id = @id",
+                                                                       new { id });
+                return deletedUser;
+            }
+        }
+    }
+}
+
