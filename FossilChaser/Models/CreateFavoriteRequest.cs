b6ce7f05a2900d648bb39e589e5d601c191cbf23
Michelle Beshears <meeshysu@gmail.com>
2019-07-19 20:57:16 -0500

    folders set up

---
+++ FossilChaser/Models/CreateFavoriteRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FossilChaser.Models
+{
+    public class CreateFavoriteRequest
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FossilId { get; set; }
+        public int FormationId { get; set; }
+    }
+}
