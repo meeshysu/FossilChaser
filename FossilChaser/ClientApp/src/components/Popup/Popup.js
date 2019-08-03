ad1f2f5b958ac3a2e957d54116f1a320badc3da1
Michelle Beshears <meeshysu@gmail.com>
2019-08-03 13:33:14 -0500

    omg

--- FossilChaser/ClientApp/src/components/Popup/Popup.js
+++ FossilChaser/ClientApp/src/components/Popup/Popup.js
@@ -18,14 +18,12 @@ render() {
   const { formation } = this.props;
 
   return (
-    <Popup className='pop-up'>
-      <div className='popupDiv'>
+    <Popup>
         FormationName={formation.formationName}
         Location={formation.location}
-        
-      </div>
-      <StarButton key={formation.id} formation={formation}></StarButton>
+        <StarButton key={formation.id} formation={formation}></StarButton>
     </Popup>
+
   )
 }
 }
